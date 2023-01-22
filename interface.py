da3b5d33fbaa671a2e7d870d0f0a7fe644fa87b2
LPOYO <11901536+romaingyh@users.noreply.github.com>
2023-01-22 21:40:23 +0100

    Ajout de l'interface graphique

--- interface.py
+++ interface.py
@@ -7,7 +7,6 @@ import customtkinter
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
